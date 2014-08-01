9dcd3c33487d6e2d6f9d02d4f07b547b98d3a012
Eric Rochester <erochest@virginia.edu>
2014-08-01 15:41:18 -0400

    ping function

--- src/Database/Orchestrate/Utils.hs
+++ src/Database/Orchestrate/Utils.hs
@@ -1,11 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 
 module Database.Orchestrate.Utils
     ( ping
     ) where
 
 
+import           Control.Lens
+import           Data.Monoid
+import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as E
+import           Network.Wreq
+
 import           Database.Orchestrate.Types
 
 
-ping :: OrchestrateT m ()
-ping = undefined
+ping :: OrchestrateIO ()
+ping = do
+    Session{..} <- ask
+    r <- fmap (view (responseStatus . statusCode))
+        . io
+        . headWith (defaults & auth .~ basicAuth (E.encodeUtf8 sessionKey) "")
+        . T.unpack
+        $ mconcat [sessionURL, "/v", T.pack $ show sessionVersion]
+    if r == 200
+        then return ()
+        else throwError $ "Invalid response: " <> (T.pack . show) r
